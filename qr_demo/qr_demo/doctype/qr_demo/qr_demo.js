cf68e5c98ade9605796acff6de79d48d979441b1
barredterra <14891507+barredterra@users.noreply.github.com>
2022-07-26 13:35:39 +0200

    feat: doctype QR Demo

---
+++ qr_demo/qr_demo/doctype/qr_demo/qr_demo.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('QR Demo', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ qr_demo/qr_demo/doctype/qr_demo/qr_demo.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2022, ALYF GmbH and contributors
+# For license information, please see license.txt
+
+from base64 import b64encode
+from io import BytesIO
+
+import qrcode
+
+from frappe.model.document import Document
+
+
+class QRDemo(Document):
+	def validate(self):
+		self.qr_code = get_qr_code(self.title, format="PNG")
+
+
+def get_qr_code(data, format: str):
+	qr_code_bytes = get_qr_code_bytes(data, format)
+	base_64_string = bytes_to_base64_string(qr_code_bytes)
+
+	return add_file_info(base_64_string)
+
+
+def add_file_info(data: str) -> str:
+	"""Add info about the file type and encoding.
+	
+	This is required so the browser can make sense of the data."""
+	return f"data:image/png;base64, {data}"
+
+
+def get_qr_code_bytes(data, format: str):
+	"""Create a QR code and return the bytes."""
+	img = qrcode.make(data)
+
+	buffered = BytesIO()
+	img.save(buffered, format=format)
+
+	return buffered.getvalue()
+
+
+def bytes_to_base64_string(data: bytes) -> str:
+	"""Convert bytes to a base64 encoded string."""
+	return b64encode(data).decode("utf-8")
